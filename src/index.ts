37f1938eefa2caca8cdb5af56b6a4ad40affc713
Lucas Souza <t.lucas.souza@self.inc>
2022-02-01 11:08:30 -0300

    feat: create app with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('teste')
+import "reflect-metadata"
+import express from "express";
+
+import { router as entityRouter } from './services/entity/controller'
+
+const PORT = 3000
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.use('/entities', entityRouter)
+
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`)
+})
